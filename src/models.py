c6273a87a46b4eef0e935f88996e669183b29ac6
Nicolás Aranguren Turmequé <nicolasarangurenturmeque@Laptop-de-Nicolas.local>
2025-11-26 22:46:58 -0500

    Cambio de nombre a los modelos.

--- src/models.py
+++ src/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.schema import ForeignKey
 from sqlalchemy.sql.expression import text
 from sqlalchemy.orm import relationship
 
-class User(Base):
+class Usuario(Base):
     __tablename__ = "usuarios"     # Nombre que queremos dar a la tabla en postgres.
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
@@ -23,7 +23,7 @@ class Post(Base):
     owner = relationship("User") # Retorna la clase de otro modelo (Usuario). Trae los atributos de Usuario con base en owner_id.
     
     
-class Vote(Base):
+class Voto(Base):
     __tablename__ = "votos"
     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True,nullable=False) # Composite key
     user_id = Column(Integer, ForeignKey("usuarios.id", ondelete="CASCADE"), primary_key=True,nullable=False) # Composite key
